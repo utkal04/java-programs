bf9a4efdec64b5ecc477d8487f564396cf2abf56
utkal04 <132186664+utkal04@users.noreply.github.com>
2023-06-30 13:46:10 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class f1
+{
+public static void main(String args[])
+{
+int var1=1064;
+float var2=55.54f;
+char var3='A';
+String var4="utkal";
+boolean var5=5>2;
+double var6=46688.33;
+long var7=943822003;
+System.out.println("THE DATATYPES ARE :");
+System.out.println("INTEGER:"+var1);
+System.out.println("FLOAT:"+var2);
+System.out.println("CHARACTER:"+var3);
+System.out.println("STRING:"+var4);
+System.out.println("BOOLEAN:"+var5);
+System.out.println("DOUBLE:"+var6);
+System.out.println("LONG:"+var7);
+}
+}
