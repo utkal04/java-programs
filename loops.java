b34411b612e3cb817aaac206ef92c751c9e8dd61
utkal04 <132186664+utkal04@users.noreply.github.com>
2023-07-01 14:02:41 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class f3
+{
+public static void main(String args[])
+{
+//FOR LOOP
+System.out.println("FOR");
+for(int i=1;i<=10;i++)
+{
+System.out.println(i);
+}
+//WHILE LOOP
+System.out.println("WHILE");
+int a=1;
+while(a<=5)
+{
+System.out.println(a);
+a++;
+}
+//DO WHILE
+int b=1;
+System.out.println("DO WHILE");
+do
+{
+System.out.println(b);
+b++;
+}
+while(b<=5);
+}
+}
