16babf589df522da4d76e258826e3caf7ec71423
utkal04 <132186664+utkal04@users.noreply.github.com>
2023-07-01 13:58:29 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class f9
+{
+public static void main(String args[])
+{
+int a=10,b=9;
+if(a>b)
+{
+System.out.println("A IS GREATER");
+}
+else
+{
+System.out.println("B IS GREATER ");
+}
+}
+}
