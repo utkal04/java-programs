38e9deb3e4465dfd7c72e49ce07dd7892c8d2ef3
utkal04 <132186664+utkal04@users.noreply.github.com>
2023-07-01 14:15:14 +0530

    Add files via upload

---
+++ array2d.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class f5
+{
+public static void main(String args[])
+{
+//2D ARRAY WHERE VALUES ALRERADY INITIALIZED
+int a[][]={{1,2,3},{4,5}};
+System.out.println(a[1][1]);
+}
+}
